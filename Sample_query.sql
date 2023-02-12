546a55060df6429dad4529984d23e162b915195c
Vee <vaibhav2145@gmail.com>
2023-02-12 11:14:23 -0500

    ppt and data source

---
+++ Sample_query.sql
@@ -0,0 +1,28 @@
+drop table activity;
+
+CREATE table activity
+(
+user_id varchar(20),
+event_name varchar(20),
+event_date date,
+country varchar(20)
+);
+
+insert into activity values (1,'app-installed','2022-01-01','India')
+,(1,'app-purchase','2022-01-02','India')
+,(2,'app-installed','2022-01-01','USA')
+,(3,'app-installed','2022-01-01','USA')
+,(3,'app-purchase','2022-01-03','USA')
+,(4,'app-installed','2022-01-03','India')
+,(4,'app-purchase','2022-01-03','India')
+,(5,'app-installed','2022-01-03','SL')
+,(5,'app-purchase','2022-01-03','SL')
+,(6,'app-installed','2022-01-04','Pakistan')
+,(6,'app-purchase','2022-01-04','Pakistan');
+
+
+select * from activity;
+
+
+select event_name, country, count(event_name) as total_events from activity group by  event_name, country;
+
